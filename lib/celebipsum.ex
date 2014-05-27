f5fd17c0bdc72c0bacd5c5987dc9426b42dad5ba
Sean Levin <slevin@yahoo-inc.com>
2014-05-27 10:57:46 -0400

    parses the corpus

--- lib/celebipsum.ex
+++ lib/celebipsum.ex
@@ -6,4 +6,32 @@ defmodule Celebipsum do
   def start(_type, _args) do
     Celebipsum.Supervisor.start_link
   end
+
+  def parse(in_string) do
+    string_to_dict(in_string, %{})
+  end
+
+  def string_to_dict(in_string, current_dict) do
+    case String.split(in_string, ~r{\s+}, parts: 4, trim: true) do
+      [first, second, third, rest] ->
+        new_dict = Dict.put(%{}, "#{first} #{second}", [String.strip(third)])
+        rest_dict = string_to_dict("#{second} #{third} #{rest}", new_dict)
+        markovmerge(current_dict, rest_dict)
+      [first, second, rest] ->
+        rest_dict = Dict.put(%{}, "#{first} #{second}", [String.strip(rest)])
+        markovmerge(current_dict, rest_dict)
+      _ ->
+        markovmerge(current_dict, %{})
+    end
+  end
+
+  def markovmerge(d1, d2) do
+    Dict.merge(d1, d2, fn _k, v1, v2 -> v1 ++ v2 end)
+  end
+
+  def readcorpus do
+    {:ok, in_string} = File.read "corpus.txt"
+    parse(in_string)
+  end
+
 end
--- test/celebipsum_test.exs
+++ test/celebipsum_test.exs
@@ -1,10 +1,78 @@
 defmodule CelebipsumTest do
   use ExUnit.Case
 
-  import C
-  test "text stream parser returns first word" do
-    first = first_word("one two three")
-    assert first == "one"
+  import Celebipsum
+
+  test "empty strings are skipped" do
+    res = parse("")
+    assert Dict.equal?(res, %{})
   end
+
+  test "single words are skipped" do
+    res = Celebipsum.parse("one")
+    assert Dict.equal?(res, %{})
+  end
+
+  test "double words are skipped" do
+    res = Celebipsum.parse("one two")
+    assert Dict.equal?(res, %{})
+  end
+
+  test "triples are stored in a dict" do
+    res = Celebipsum.parse("one two three")
+    assert Dict.equal?(res, %{"one two" => ["three"]})
+  end
+
+  test "merge builds lists" do
+    d1 = %{"one two" => ["a"]}
+    d2 = %{"one two" => ["b"]}
+    res = markovmerge(d1, d2)
+    assert res ===  %{"one two" => ["a", "b"]}
+  end
+
+  test "second tripple also stored in same dict" do
+    res = parse("one two three four")
+    answer = %{"one two" => ["three"], "two three" => ["four"]}
+    assert Dict.equal?(res, answer), res, answer, "dicts not equal"
+  end
+
+  test "merges duplicates" do
+    res = parse("one one one two")
+    answer = %{"one one" => ["one", "two"]}
+    assert Dict.equal?(res, answer), res, answer, "dicts not equal"
+  end
+
+  test "ignores extra whitespace" do
+    res = parse("\n  one  two\n\t  three   ")
+    answer = %{"one two" => ["three"]}
+    assert Dict.equal?(res, answer), res, answer, "dicts not equal"
+  end
+
+# test that same initals are stored in list if different
+# and if same
+
+# test parsing io stream in
+
+# test picking random
+
+# test picking random next based on previous input
+
+# test generating a result set based on input
+
+
+
+  # test "words are stored decapitalized" do
+  #   res = Celebipsum.parse("One Two Three")
+  #   assert Dict.equal?(res, %{"one two" => ["three"]})
+  # end
+
+
+
+  # compare two hashdicts
+
+  # test "text stream parser returns first word" do
+  #   first = first_word("one two three")
+  #   assert first == "one"
+  # end
 
 end
