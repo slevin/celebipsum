e685305af6c15cce192c7ee23265cbaa381f4a24
Sean Levin <sean.levin@gmail.com>
2014-06-04 23:40:33 -0400

    much faster parsing of corpus
    removed the recursive string/dict building
    and use Enum iterator methods instead

--- lib/reader.ex
+++ lib/reader.ex
@@ -1,34 +1,18 @@
 defmodule Celebipsum.Reader do
 
   def parse(in_string) do
-    string_to_dict(in_string, %{})
-  end
-
-  def string_to_dict(in_string, current_dict) do
-    case String.split(in_string, ~r{\s+}, parts: 4, trim: true) do
-      [first, second, third, rest] ->
-        new_dict = dict_from_triple(first, second, third)
-        rest_dict = string_to_dict("#{second} #{third} #{rest}", new_dict)
-        markovmerge(current_dict, rest_dict)
-      [first, second, third] ->
-        rest_dict = dict_from_triple(first, second, third)
-        markovmerge(current_dict, rest_dict)
-      _ ->
-        markovmerge(current_dict, newdict())
-    end
-  end
-
-  def dict_from_triple(first, second, third) do
-    key = [first, second]
-    Dict.put(newdict(), key, [String.strip(third)])
+    String.split(in_string, ~r{\s+}, strip: true)
+    |> Enum.chunk(3,1)
+    |> Enum.map(fn([first, second, third]) ->
+                    Dict.put(newdict, [first, second], [String.strip(third)])
+                end)
+    |> Enum.reduce(fn(current, acc) ->
+                       Dict.merge(acc, current, fn(_k, l1, l2) -> l1 ++ l2 end)
+                   end)
   end
 
   def newdict() do
     HashDict.new()
-  end
-
-  def markovmerge(d1, d2) do
-    Dict.merge(d1, d2, fn _k, v1, v2 -> v1 ++ v2 end)
   end
 
   def readcorpus do
